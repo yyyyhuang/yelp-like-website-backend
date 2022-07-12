c40e944d47f7fdac0f3d323bb0c2eeb08f369862
yyyyhuang <yingjie.huang2013@gmail.com>
2022-07-12 17:03:06 -0400

    remove lines

--- api/favorites.controller.js
+++ api/favorites.controller.js
@@ -14,9 +14,6 @@ export default class FavoritesController {
                 res.status(500).json({ error });
             }
 
-            if (FavoritesResponse.modifiedCount === 0) {
-                throw new Error ("Unable to update favorites.")
-            }
             res.json({ status: "success"});
         } catch(e) {
             res.status(500).json({ error: e.message })
